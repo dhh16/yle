d722f53aba8f7225b9feca27374fe9d368056c4c
esohman <emily.ohman@gmail.com>
2016-05-19 10:50:13 +0300

    Update NER.py

--- data_analysis/NER.py
+++ data_analysis/NER.py
@@ -1,10 +1,6 @@
 from polyglot.text import Text
-import numpy as np
-from numpy import loadtxt
-import nltk
 import csv
 from collections import defaultdict
-
 
 columns = defaultdict(list) 
 with open('yle.csv') as f:
@@ -14,9 +10,7 @@ with open('yle.csv') as f:
 			columns[k].append(v)
 ids = columns.get('id')
 contents = columns.get('content')
-
 data = {id: row for id, row in zip(ids, contents)}
-
 
 #output = csv.writer(open("yletemp.csv", "wb"))
 #output.writerow(('id', 'contents','NE','tag'))
@@ -32,4 +26,3 @@ for id in data:
  		pass
 text_file.write(output)
 text_file.close()
-
